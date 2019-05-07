d9bb2cba2b5cfd7f2c2e6240eaba4ce5e4fbd13a
Sergio Rosa <scarlosjazz@gmail.com>
2019-05-07 11:05:28 -0400

    Adding Cards-Tabs

--- src/pages/Main/index.js
+++ src/pages/Main/index.js
@@ -1,13 +1,43 @@
 import React from 'react';
 import Header from '~/components/Header';
+import Icon from 'react-native-vector-icons/MaterialIcons';
 import Tabs from '~/components/Tabs';
 
-import { Container } from './styles';
+import {
+  Container,
+  Content,
+  Card,
+  CardHeader,
+  CardContent,
+  CardFooter,
+  Title,
+  Description,
+  Annotation,
+} from './styles';
 
 export default function Main() {
   return (
     <Container>
       <Header />
+
+      <Content>
+        <Card>
+          <CardHeader>
+            <Icon name="attach-money" size={28} color="777" />
+            <Icon name="visibility-off" size={28} color="777" />
+          </CardHeader>
+          <CardContent>
+            <Title>Available to withdraw</Title>
+            <Description> $ 5.280.876,65</Description>
+          </CardContent>
+          <CardFooter>
+            <Annotation>
+              Transfer of U$25,000 received from Sergio Carlos Rosa today at 06:00pm
+            </Annotation>
+          </CardFooter>
+        </Card>
+      </Content>
+
       <Tabs />
     </Container>
   );
